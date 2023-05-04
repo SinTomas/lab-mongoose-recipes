845f3fc7b7c99282f25bee2ea37c218cde47a6e9
Tomas Maria <tomaslbpmaria@hotmail.com>
2023-05-04 17:02:45 +0100

    Lab Done

--- index.js
+++ index.js
@@ -1,30 +1,74 @@
-const mongoose = require('mongoose');
+let mongoose = require('mongoose');
 
 // Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require('./models/Recipe.model');
+let Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
-const data = require('./data');
+let data = require('./data');
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+let MONGODB_URI = 'mongodb://127.0.0.1:27017/recipe-app';
 
 //Method 1 : Using Async Await
 
-const manageRecipes = async () => {
+let manageRecipes = async () => {
   try {
     // Connection to the database "recipe-app"
-    const dbConnection = await mongoose.connect(MONGODB_URI);
+    let dbConnection = await mongoose.connect(MONGODB_URI);
     console.log(`Connected to the database: "${dbConnection.connection.name}"`);
 
     // Before adding any recipes to the database, let's remove all existing ones
     await Recipe.deleteMany();
 
     // Run your code here, after you have insured that the connection was made
+    let bacalhauBras = {
+      title: "Bacalhau à Brás",
+      level: "Easy Peasy",
+      ingredients: ["Codfish", "Onions", "garlic", "potatoes", "eggs", "milk", "olives"],
+      cuisine: "Portuguese",
+      dishType: "main_course",
+      image: "https://www.pingodoce.pt/wp-content/uploads/2016/03/bacalhau-a-bras.jpg",
+      duration: 30,
+      creator: "Senhor Brás"
+    };
+    await Recipe.create(bacalhauBras);
+   
+    let arrayOfRecipes = await Recipe.insertMany(data);
+    console.log(arrayOfRecipes);
+
+    await Recipe.findOneAndUpdate({title:"Rigatoni alla Genovese"}, {duration:100});
+
+    await Recipe.deleteOne({title: "Carrot Cake"});
+
+    mongoose.connection.close(function() {console.log('Mongoose connection closed')});
+
   } catch (error) {
     console.log(error);
   }
 };
 
 manageRecipes();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 //Method 2: Using .then() method
 //If you want to use this method uncomment the code below:
